2766257b8ac6643ad80e06efe7bb41da984c9229
Peter-32 <peter@impactradius.com>
2016-07-03 12:03:30 -0700

    stooge sort added and working

---
+++ stooge sort/stooge sort tests/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using stooge_sort;
+using System.Collections.Generic;
+namespace stooge_sort_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Assemble
+            ProgramLogic programLogic = new ProgramLogic();
+            List<int> list2a = new List<int> { 2, 1 };
+            List<int> list2b = new List<int> { 1, 2 };
+            List<int> list9a = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<int> list9b = new List<int> { 5, 6, 4, 7, 3, 8, 2, 9, 1 };
+            List<int> list9c = new List<int> { 7, 3, 2, 1, 5, 4, 6, 8, 9 };
+            List<int> list9d = new List<int> { 5, 6, 4, 7, 8, 9, 1, 2, 3 };
+            List<int> list9e = new List<int> { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            List<int> list9f = new List<int> { 1, 3, 5, 7, 9, 2, 4, 6, 8 };
+            List<int> list9g = new List<int> { 9, 7, 5, 3, 1, 2, 4, 6, 8 };
+            List<int> listEmpty = new List<int>();
+            List<int> list1a = new List<int> { 1 };
+
+            // Act
+            programLogic.stoogeSort(list2a);
+            programLogic.stoogeSort(list2b);
+            programLogic.stoogeSort(list9a);
+            programLogic.stoogeSort(list9b);
+            programLogic.stoogeSort(list9c);
+            programLogic.stoogeSort(list9d);
+            programLogic.stoogeSort(list9e);
+            programLogic.stoogeSort(list9f);
+            programLogic.stoogeSort(list9g);
+            programLogic.stoogeSort(listEmpty);
+            programLogic.stoogeSort(list1a);
+
+            // Assert
+            CollectionAssert.AreEqual(list2a, new List<int> { 1, 2 });
+            CollectionAssert.AreEqual(list2b, new List<int> { 1, 2 });
+            CollectionAssert.AreEqual(list9a, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            CollectionAssert.AreEqual(list9b, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            CollectionAssert.AreEqual(list9c, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            CollectionAssert.AreEqual(list9d, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            CollectionAssert.AreEqual(list9e, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            CollectionAssert.AreEqual(list9f, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            CollectionAssert.AreEqual(list9g, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            CollectionAssert.AreEqual(listEmpty, new List<int> { });
+            CollectionAssert.AreEqual(list1a, new List<int> { 1 });
+        }
+    }
+}
---
+++ stooge sort/stooge sort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stooge_sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgramLogic programLogic = new ProgramLogic();
+            List<int> list9e = new List<int> { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            programLogic.stoogeSort(list9e);
+            list9e.ForEach(Console.WriteLine);
+            Console.ReadKey();
+        }
+    }
+
+    public class ProgramLogic
+    {
+        public void execute()
+        {
+
+        }
+
+        // swap elements.  If length is > 2 then do stooge sort three times; first on the first 2/3
+        // then on the last 2/3; then on the beginning 2/3
+
+        public void stoogeSort(List<int> list)
+        {
+            int listLength = list.Count();
+            stoogeSortLoop(list, 0, listLength - 1);
+        }
+
+        public void stoogeSortLoop(List<int> list, int startIdx, int endIdx)
+        {
+            int listLength = endIdx - startIdx + 1;
+            if (listLength < 2)
+                return;
+            if (list[endIdx] < list[startIdx])
+            {
+                int temp = list[startIdx];
+                list[startIdx] = list[endIdx];
+                list[endIdx] = temp;
+            }
+
+            if (listLength > 2) // if size is at least 3
+            {                
+                int twoThirdLength = (listLength + 1) * 2 / 3; // the +1 will end up being +2 when multiplied by 2.  This rounds the divided by 3 up.
+                stoogeSortLoop(list, startIdx, startIdx + twoThirdLength - 1);
+                stoogeSortLoop(list, endIdx - twoThirdLength + 1 , endIdx);
+                stoogeSortLoop(list, startIdx, startIdx + twoThirdLength - 1);
+            }
+        }
+    }
+}
