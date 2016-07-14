8f7b935fda5fc5fc3e6871fa7b9a8ef95dfa99f4
Peter Myers <peter@impactradius.com>
2016-07-14 00:05:05 -0700

    Largest Palindrome in a Sentence

---
+++ Efficient Largest Palindrome/Efficient Largest Palindrome/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Efficient_Largest_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "search for a palindrome in a sentence";
+            Console.WriteLine("\"" + PalindromeUtil.largestPalindrome(str) + "\"");
+            Console.ReadKey();
+        }
+    }
+    public static class PalindromeUtil
+    {
+        // work with double letters first, find the locations
+        // check around each one for more palindrome letters
+        // afterwards look for patterns like ete (single letter in middle); 
+        // then with those search the letters around them
+        public static string largestPalindrome(string str)
+        {
+            if (str.Length < 2) { return str; }
+            str = str.ToLower();
+
+            // The Tuple represents startIdx, length
+            int idxLargestPalindromeStart = 0;
+            int largestPalindromeSize = 1;
+            // DOUBLE LETTER CENTER
+            for (var i = 1; i < str.Length; i++)
+            {
+                if (str[i] == str[i - 1])
+                {
+                    for (var j = 1; ; j++)
+                    {
+                        // check for out of bound exception
+                        if (i + j >= str.Length || i - 1 - j < 0)
+                        {
+                            // check if largest palindrome so far
+                            if (2 * j > largestPalindromeSize)
+                            {
+                                largestPalindromeSize = 2 * j;
+                                idxLargestPalindromeStart = i - j;
+                            }
+                            break;
+                        }
+                        // Can't combine these because of out of bound exception
+                        if (str[i + j] != str[i - 1 - j])
+                        {
+                            // check if largest palindrome so far
+                            if (2 * j > largestPalindromeSize)
+                            {
+                                largestPalindromeSize = 2 * j;
+                                idxLargestPalindromeStart = i - j;
+                            }
+                            break;
+                        }
+                        // Do nothing here
+                    }
+                }
+            }
+
+            // SINGLE LETTER CENTER
+            for (var i = 0; i < str.Length - 2; i++)
+            {
+                if (str[i] == str[i + 2])
+                {
+                    for (var j = 1; ; j++)
+                    {
+                        // check for out of bound exception
+                        if (i + 2 + j >= str.Length || i - j < 0)
+                        {
+                            // check if largest palindrome so far
+                            if (1 + 2 * j > largestPalindromeSize)
+                            {
+                                largestPalindromeSize = 1 + 2 * j;
+                                idxLargestPalindromeStart = i + 1 - j;
+                            }
+                            break;
+                        }
+                        // Can't combine these because of out of bound exception
+                        if (str[i + 2 + j] != str[i - j])
+                        {
+                            // check if largest palindrome so far
+                            if (1 + 2 * j > largestPalindromeSize)
+                            {
+                                largestPalindromeSize = 1 + 2 * j;
+                                idxLargestPalindromeStart = i + 1 - j;
+                            }
+                            break;
+                        }
+                        // Do nothing here
+                    }
+                }
+            }
+            return str.Substring(idxLargestPalindromeStart,largestPalindromeSize);
+        }
+    }
+
+}
---
+++ Largest Mirror Match with Hole/Largest Mirror Match with Hole/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_Mirror_Match_with_Hole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "testabcdtset";
+            Console.WriteLine(MirrorStringUtil.largestMirrorMatchWithHole(str));
+            Console.ReadKey();
+        }
+    }
+
+    public static class MirrorStringUtil
+    {
+        // Reverse the string
+        // compare the two strings index by index
+        // find the longest chain of indices matching
+        // return the string of those indices
+        public static string largestMirrorMatchWithHole(string str)
+        {
+            if (str.Length <= 1) { return str; }
+            str = str.ToLower();
+            IEnumerable<char> reverseStr = str.Reverse();
+            int idxLongestChainMatching = 0;
+            int longestChain = 1;
+            int currentChainLength = 0;
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (str[i] == reverseStr.ElementAt(i))
+                {
+                    currentChainLength++;
+                }
+                else
+                {
+                    if (currentChainLength > longestChain)
+                    {
+                        longestChain = currentChainLength;
+                        idxLongestChainMatching = i - currentChainLength;
+                    }
+                    currentChainLength = 0;
+                } // jaspsaj
+            }
+            if (currentChainLength > longestChain)
+            {
+                longestChain = currentChainLength;
+                idxLongestChainMatching = str.Length - currentChainLength;
+            }
+            return str.Substring(idxLongestChainMatching, longestChain);
+        }
+    }
+}
